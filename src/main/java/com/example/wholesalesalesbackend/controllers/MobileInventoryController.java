d53174c8c25ca1d05766a03754fe5c2b37582a07
tusharjain8465 <tusharjain8465@gmail.com>
2026-01-15 13:58:26 +0530

    Update MobileInventoryController.java

--- src/main/java/com/example/wholesalesalesbackend/controllers/MobileInventoryController.java
+++ src/main/java/com/example/wholesalesalesbackend/controllers/MobileInventoryController.java
@@ -14,7 +14,7 @@ import com.example.wholesalesalesbackend.service.MobileInventoryService;
 
 @RestController
 @RequestMapping("/api/mobile-inventory")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://arihant-wholesale-shop-frontend.vercel.app")
 public class MobileInventoryController {
 
     @Autowired
